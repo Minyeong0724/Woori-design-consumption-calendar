2ccaeb5ebed26ab5ee977925b89add37574d8a54
Minyeong <skaalsdud7@gmail.com>
2026-01-14 10:52:26 +0900

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ShowTotalPrice from './components/consumptions/showTotalPrice'
+
+function App() {
+
+  return (
+    <>
+      <ShowTotalPrice />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/consumptions/ShowTotalPrice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ShowTotalPrice = (  ) => {
+    
+  return (
+    <p> This is show Total price</p>
+  )
+}
+
+export default ShowTotalPrice; 
+
