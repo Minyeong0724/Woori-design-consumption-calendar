d5b871ae91145130e5687ac5663e4abb11b1f486
Minyeong <skaalsdud7@gmail.com>
2026-01-14 12:43:37 +0900

    add ShowTotalPrices.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import './App.css'
 import ShowTotalPrice from './components/consumptions/showTotalPrice'
 
+const dummyData = [
+    {
+        id: 1, 
+        title: '식당',
+        price: 13000,
+        year: 2026,
+        month: 1,
+        day: 2
+    },
+    {
+        id: 2, 
+        title: '카페',
+        price: 3000,
+        year: 2026,
+        month: 1,
+        day: 21
+    },
+    {
+        id: 3, 
+        title: '교통',
+        price: 5000, 
+        year: 2026,
+        month: 4,
+        day: 30
+    },
+    {
+        id: 4, 
+        title: '오락',
+        price: 4000, 
+        year: 2025,
+        month: 1,
+        day: 29
+    }
+]
+
+const todayDate = {year: 2026, month: 1, day: 14};
+
 function App() {
 
   return (
     <>
-      <ShowTotalPrice />
+      <ShowTotalPrice dummyData = {dummyData} todayDate ={todayDate}/>
     </>
   )
 }
